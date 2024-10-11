2cde7c91909def16b2cfea116529fb47deecb896
Johannes <johannes-programming@mailfence.com>
2024-10-11 15:47:08 +0200

    u

--- src/v440/_utils/VList.py
+++ src/v440/_utils/VList.py
@@ -3,6 +3,12 @@ from v440._utils.Base import Base
 from typing import *
 
 class VList(Base, OkayList):
+    def __iadd__(self, other, /):
+        self._data += type(self)(other)._data
+        return self
+    def __imul__(self, other, /):
+        self.data = self.data * other
+        return self
     def __init__(self, data=None) -> None:
         self.data = data
     def __sorted__(self, /, **kwargs) -> Self:
