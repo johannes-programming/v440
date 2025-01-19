64b7ae4fce5c163c2299db1bcfa9562f2c8466a6
Johannes <johannes-programming@mailfence.com>
2025-01-19 22:57:55 +0100

    Version 1.0.20

--- src/v440/core/Pre.py
+++ src/v440/core/Pre.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-import datahold
+from typing import *
+
 import keyalias
 
 from v440._utils import QualifierParser
-from v440._utils.Base import Base
 from v440._utils.VList import VList
 
 __all__ = ["Pre"]
@@ -13,21 +13,27 @@ __all__ = ["Pre"]
 @keyalias.keyalias(phase=0, subphase=1)
 class Pre(VList):
 
-    def __init__(self, data=None):
+    data: list
+    phase: Any
+    subphase: Any
+
+    def __init__(self: Self, data: Any = None) -> None:
         self.data = data
 
-    def __str__(self) -> str:
-        if self.isempty():
-            return ""
-        return self.phase + str(self.subphase)
+    def __str__(self: Self) -> str:
+        ans: str = ""
+        if not self.isempty():
+            ans += self.phase
+            ans += str(self.subphase)
+        return ans
 
     @property
-    def data(self) -> list:
+    def data(self: Self) -> list:
         return list(self._data)
 
     @data.setter
-    def data(self, value):
+    def data(self: Self, value: Any) -> None:
         self._data = QualifierParser.PRE(value)
 
-    def isempty(self) -> bool:
+    def isempty(self: Self) -> bool:
         return self._data == [None, None]
