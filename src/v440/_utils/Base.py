e0a1b72857af7e87bfcd002bd7be533bce5408aa
Johannes <johannes-programming@mailfence.com>
2025-09-04 20:54:23 +0200

    t

--- src/v440/_utils/Base.py
+++ src/v440/_utils/Base.py
@@ -8,6 +8,7 @@ from v440.core.VersionError import VersionError
 class Base:
 
     def __eq__(self: Self, other: Any) -> bool:
+        "This magic method implements self==other."
         ans: bool
         try:
             opp: Self = type(self)(other)
@@ -18,6 +19,7 @@ class Base:
         return ans
 
     def __ge__(self: Self, other: Any, /) -> bool:
+        "This magic method implements self>=other."
         ans: bool
         try:
             opp: Self = type(self)(other)
@@ -31,6 +33,7 @@ class Base:
     __hash__ = OkayABC.__hash__
 
     def __le__(self: Self, other: Any, /) -> bool:
+        "This magic method implements self<=other."
         ans: bool
         try:
             opp: Self = type(self)(other)
@@ -45,6 +48,7 @@ class Base:
     __repr__ = OkayABC.__repr__
 
     def __setattr__(self: Self, name: str, value: Any) -> None:
+        "This magic method implements setattr(self, name, value)."
         if name.startswith("_"):
             object.__setattr__(self, name, value)
             return
