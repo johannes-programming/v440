900f9c0e2fd0dd2baeebe0e453250fcd25d5a506
Johannes <johannes.programming@gmail.com>
2025-09-17 16:49:24 +0200

    Version 1.0.29

--- src/v440/_utils/utils.py
+++ src/v440/_utils/utils.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import functools
 import operator
 import string
 import types
@@ -7,34 +8,67 @@ from typing import *
 
 from v440.core.VersionError import VersionError
 
-SEGCHARS = string.ascii_lowercase + string.digits
 
+class Digest:
+    __slots__ = ("lookup", "name")
+    lookup: dict
+    name: str
 
-def digest(old: Any, /) -> types.FunctionType:
-    byNone: Any = getattr(old, "byNone", None)
-    byInt: Any = getattr(old, "byInt", None)
-    byList: Any = getattr(old, "byList", None)
-    byStr: Any = getattr(old, "byStr", None)
-
-    def new(*args: Any, **kwargs: Any) -> Any:
-        args: list = list(args)
-        value: Any = args.pop()
-        ans: Any
-        if value is None:
-            ans = byNone(*args, **kwargs)
-        elif isinstance(value, int):
-            value = int(value)
-            ans = byInt(*args, value, **kwargs)
-        elif isinstance(value, str) or not hasattr(value, "__iter__"):
-            value = str(value).lower().strip()
-            ans = byStr(*args, value, **kwargs)
-        else:
-            value = list(value)
-            ans = byList(*args, value, **kwargs)
+    def __call__(self: Self, *args: Any, **kwargs: Any) -> Any:
+        "This magic method implements self(*args, **kwargs)."
+        args0: list = list(args)
+        value: Any = args0.pop()
+        key: Any = self._getkey(value)
+        if key is int:
+            args0.append(int(value))
+        if key is str:
+            args0.append(str(value).lower().strip())
+        if key is list:
+            args0.append(list(value))
+        ans: Any = self.lookup[key](*args0, **kwargs)
         return ans
 
-    new.__name__ = old.__name__
-    return new
+    def __get__(
+        self: Self,
+        *args: Any,
+        **kwargs: Any,
+    ) -> types.FunctionType | types.MethodType:
+        "This magic method implements getting as an attribute from a class or an object."
+
+        def new(*args_: Any, **kwargs_: Any) -> Any:
+            return self(*args_, **kwargs_)
+
+        new.__name__ = self.name
+        ans: Any = new.__get__(*args, **kwargs)
+        return ans
+
+    def __init__(self: Self, name: str) -> None:
+        "This magic method sets up self."
+        self.lookup = dict()
+        self.name = name
+
+    @classmethod
+    def _getkey(cls: type, value: Any) -> Any:
+        if value is None:
+            return
+        if isinstance(value, int):
+            return int
+        if isinstance(value, str):
+            return str
+        try:
+            value.__iter__
+        except AttributeError:
+            return str
+        else:
+            return list
+
+    def _overload(self: Self, key: Any, value: Any) -> Self:
+        self.lookup[key] = value
+        overload(value)
+        return self
+
+    def overload(self: Self, key: Any = None) -> functools.partial:
+        return functools.partial(type(self)._overload, self, key)
 
 
 def literal(value: Any, /) -> str:
@@ -64,27 +98,31 @@ def segment(value: Any, /) -> Any:
         raise e from None
 
 
-@digest
-class _segment:
-    def byNone() -> None:
-        return
+_segment: Digest = Digest("_segment")
 
-    def byInt(value: Any, /) -> Any:
-        if value < 0:
-            raise ValueError
+
+@_segment.overload()
+def _segment():
+    return
+
+
+@_segment.overload(int)
+def byInt(value: int, /) -> Any:
+    if value < 0:
+        raise ValueError
+    return value
+
+
+@_segment.overload(str)
+def _segment(value: Any, /) -> int | str:
+    if value.strip(string.ascii_lowercase + string.digits):
+        raise ValueError(value)
+    if value.strip(string.digits):
         return value
-
-    def byStr(value: Any, /) -> Any:
-        if value.strip(SEGCHARS):
-            raise ValueError(value)
-        ans: Any
-        if value.strip(string.digits):
-            ans = value
-        elif value == "":
-            ans = 0
-        else:
-            ans = int(value)
-        return ans
+    elif value == "":
+        return 0
+    else:
+        return int(value)
 
 
 def torange(key: Any, length: Any) -> range:
