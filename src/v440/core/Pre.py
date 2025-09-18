b2d68159adaf4ac26c6c82c972e1a591efd9fe7d
Johannes <johannes.programming@gmail.com>
2025-09-19 01:22:36 +0200

    Version 2.0.0.dev9

--- src/v440/core/Pre.py
+++ src/v440/core/Pre.py
@@ -4,10 +4,61 @@ from typing import *
 
 import keyalias
 
-from v440._utils import PhasedQualifierParser
+from v440._utils import utils
+from v440._utils.Digest import Digest
+from v440._utils.Pattern import Pattern
 from v440._utils.WList import WList
 
 __all__ = ["Pre"]
+
+
+PHASEDICT: dict = dict(
+    alpha="a",
+    a="a",
+    beta="b",
+    b="b",
+    preview="rc",
+    pre="rc",
+    c="rc",
+    rc="rc",
+)
+
+
+parse_data: Digest = Digest("parse_data")
+
+
+@parse_data.overload()
+def parse_data() -> list:
+    return [None, None]
+
+
+@parse_data.overload(list)
+def parse_data(value: list) -> Any:
+    l: Any
+    n: Any
+    l, n = list(map(utils.segment, value))
+    if [l, n] == [None, None]:
+        return [None, None]
+    l = PHASEDICT[l]
+    if not isinstance(n, int):
+        raise TypeError
+    return [l, n]
+
+
+@parse_data.overload(str)
+def parse_data(value: str) -> list:
+    if value == "":
+        return [None, None]
+    v: str = value
+    v = v.replace("_", ".")
+    v = v.replace("-", ".")
+    m: Any = Pattern.PARSER.bound.search(v)
+    l: Any
+    n: Any
+    l, n = m.groups()
+    l = PHASEDICT[l]
+    n = 0 if (n is None) else int(n)
+    return [l, n]
 
 
 @keyalias.keyalias(phase=0, subphase=1)
@@ -37,7 +88,7 @@ class Pre(WList):
 
     @data.setter
     def data(self: Self, value: Any) -> None:
-        self._phase, self._subphase = PhasedQualifierParser.PRE(value)
+        self._phase, self._subphase = parse_data(value)
 
     def isempty(self: Self) -> bool:
         return self._data == [None, None]
