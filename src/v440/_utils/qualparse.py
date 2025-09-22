8411a01498b034c930f9d691b827fa27c26e8582
Johannes <johannes.programming@gmail.com>
2025-09-22 02:05:46 +0200

    Version 2.0.0.dev42

--- src/v440/_utils/qualparse.py
+++ src/v440/_utils/qualparse.py
@@ -29,8 +29,8 @@ def parse_leg(value: list) -> tuple:
 def parse_leg(value: str) -> tuple:
     v = value
     pre: tuple = None, None
-    post: Any = None
-    dev: Any = None
+    post: Optional[str] = None
+    dev: Optional[str] = None
     m: Any
     x: Any
     y: Any
@@ -50,43 +50,6 @@ def parse_leg(value: str) -> tuple:
             continue
         pre = x, y
     return pre, post, dev
-
-
-parse_pre: Digest = Digest("parse_pre")
-
-
-@parse_pre.overload()
-def parse_pre() -> tuple:
-    return None, None
-
-
-@parse_pre.overload(list)
-def parse_pre(value: list) -> tuple:
-    x: Any
-    y: Any
-    x, y = map(utils.segment, value)
-    if (x, y) == (None, None):
-        return None, None
-    x = Cfg.cfg.data["phases"][x]
-    if not isinstance(y, int):
-        raise TypeError
-    return x, y
-
-
-@parse_pre.overload(str)
-def parse_pre(value: str) -> tuple:
-    if value == "":
-        return [None, None]
-    v: str = value
-    v = v.replace("_", ".")
-    v = v.replace("-", ".")
-    m: Any = Pattern.PARSER.bound.search(v)
-    l: Any
-    n: Any
-    l, n = m.groups()
-    l = Cfg.cfg.data["phases"][l]
-    n = 0 if (n is None) else int(n)
-    return l, n
 
 
 parse_dev: Digest = Digest("parse_dev")
@@ -175,3 +138,40 @@ def parse_post(value: str) -> Optional[int]:
         raise ValueError
     if y is not None:
         return int(y)
+
+
+parse_pre: Digest = Digest("parse_pre")
+
+
+@parse_pre.overload()
+def parse_pre() -> tuple:
+    return None, None
+
+
+@parse_pre.overload(list)
+def parse_pre(value: list) -> tuple:
+    x: Any
+    y: Any
+    x, y = map(utils.segment, value)
+    if (x, y) == (None, None):
+        return None, None
+    x = Cfg.cfg.data["phases"][x]
+    if not isinstance(y, int):
+        raise TypeError
+    return x, y
+
+
+@parse_pre.overload(str)
+def parse_pre(value: str) -> tuple:
+    if value == "":
+        return [None, None]
+    v: str = value
+    v = v.replace("_", ".")
+    v = v.replace("-", ".")
+    m: Any = Pattern.PARSER.bound.search(v)
+    l: Any
+    n: Any
+    l, n = m.groups()
+    l = Cfg.cfg.data["phases"][l]
+    n = 0 if (n is None) else int(n)
+    return l, n
