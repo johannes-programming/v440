5aab0e8f1c491d4a680d77e3c0f78819f312a4d8
Johannes <johannes.programming@gmail.com>
2025-10-02 21:18:09 +0200

    Version 2.0.0.dev124

--- src/v440/_utils/qualparse/__init__.py
+++ src/v440/_utils/qualparse/__init__.py
@@ -1,29 +1,28 @@
 from __future__ import annotations
 
-import functools
-import operator
-import string
-import types
+import string as string_
 from typing import *
 
-import setdoc
-
 from v440._utils.Cfg import Cfg
-from v440._utils.Pattern import Pattern
-from v440.core.VersionError import VersionError
 
 
 # parse_pre
 def parse_pre(value: Any) -> tuple:
     v: str = str(value).lower()
-    if v == "":
-        return None, None
     v = v.replace("_", ".")
     v = v.replace("-", ".")
-    m: Any = Pattern.PARSER.bound.search(v)
-    l: Any
-    n: Any
-    l, n = m.groups()
-    l = Cfg.cfg.data["phases"][l]
-    n = 0 if (n is None) else int(n)
-    return l, n
+    x: str = v.strip(string_.digits)
+    v = v[len(x) :]
+    q: bool = x.endswith(".")
+    if q:
+        if not v:
+            raise ValueError
+        x = x[:-1]
+    p: bool = x.startswith(".")
+    if p:
+        x = x[1:]
+    if x:
+        return Cfg.cfg.data["phases"][x], int("0" + v)
+    if p or v:
+        raise ValueError
+    return None, None
