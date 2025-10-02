5874f3baed80a217765b3dfc799303cc274247b6
Johannes <johannes.programming@gmail.com>
2025-10-02 21:22:57 +0200

    Version 2.0.0.dev125

--- src/v440/core/Qual.py
+++ src/v440/core/Qual.py
@@ -1,17 +1,40 @@
 from __future__ import annotations
 
 import operator
+import string as string_
 from typing import *
 
 import setdoc
 from overloadable import Overloadable
 
-from v440._utils import qualparse
+from v440._utils.Cfg import Cfg
 from v440._utils.guarding import guard
 from v440._utils.Pattern import Pattern
 from v440._utils.SlotStringer import SlotStringer
 
 __all__ = ["Qual"]
+
+
+class qualparse:
+    def parse_pre(value: Any) -> tuple:
+        v: str = str(value).lower()
+        v = v.replace("_", ".")
+        v = v.replace("-", ".")
+        x: str = v.strip(string_.digits)
+        v = v[len(x) :]
+        q: bool = x.endswith(".")
+        if q:
+            if not v:
+                raise ValueError
+            x = x[:-1]
+        p: bool = x.startswith(".")
+        if p:
+            x = x[1:]
+        if x:
+            return Cfg.cfg.data["phases"][x], int("0" + v)
+        if p or v:
+            raise ValueError
+        return None, None
 
 
 class Qual(SlotStringer):
