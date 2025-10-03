1109664353fdbaf88f0265ebabe7baf1bdecdfe2
Johannes <johannes.programming@gmail.com>
2025-10-03 11:14:40 +0200

    Version 2.0.0.dev146

--- src/v440/_utils/releaseparse/ranging.py
+++ src/v440/_utils/releaseparse/ranging.py
@@ -6,32 +6,44 @@ from typing import *
 __all__ = ["torange"]
 
 
-def torange(key: Any, length: Any) -> range:
-    start: Any = key.start
-    stop: Any = key.stop
-    step: Any = key.step
-    if step is None:
-        step = 1
-    else:
-        step = operator.index(step)
-        if step == 0:
-            raise ValueError
-    fwd: bool = step > 0
-    if start is None:
-        start = 0 if fwd else (length - 1)
-    else:
-        start = operator.index(start)
-    if stop is None:
-        stop = length if fwd else -1
-    else:
-        stop = operator.index(stop)
-    if start < 0:
-        start += length
-    if start < 0:
-        start = 0 if fwd else -1
-    if stop < 0:
-        stop += length
-    if stop < 0:
-        stop = 0 if fwd else -1
+def torange(key: Any, length: int) -> range:
+    step: Any = calcstep(key.step)
+    start: int = calcstart(key.start, length=length, fwd=step > 0)
+    stop: int = calcstop(key.stop, length=length, fwd=step > 0)
     ans: range = range(start, stop, step)
+    return ans
+
+
+def calcstep(value: Optional[SupportsIndex]) -> int:
+    if value is None:
+        return 1
+    ans: int = operator.index(value)
+    if ans == 0:
+        raise ValueError
+    return ans
+
+
+def calcstart(value: Optional[SupportsIndex], *, length: int, fwd: bool) -> int:
+    ans: int
+    if value is None:
+        ans = 0 if fwd else (length - 1)
+    else:
+        ans = operator.index(value)
+    if ans < 0:
+        ans += length
+    if ans < 0:
+        ans = 0 if fwd else -1
+    return ans
+
+
+def calcstop(value: Optional[SupportsIndex], *, length: int, fwd: bool) -> int:
+    ans: int
+    if value is None:
+        ans = length if fwd else -1
+    else:
+        ans = operator.index(value)
+    if ans < 0:
+        ans += length
+    if ans < 0:
+        ans = 0 if fwd else -1
     return ans
