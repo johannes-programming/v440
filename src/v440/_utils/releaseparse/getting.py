02e0fbd4e422b5cc343b9248c558a1bf4a4fb0fc
Johannes <johannes.programming@gmail.com>
2025-10-05 11:55:30 +0200

    Version 2.0.0.dev178

--- src/v440/_utils/releaseparse/getting.py
+++ src/v440/_utils/releaseparse/getting.py
@@ -4,29 +4,17 @@ import operator
 from functools import partial
 from typing import *
 
-from overloadable import Overloadable
-
 from v440._utils.releaseparse import ranging
 
 
-@Overloadable
-def getitem(data: tuple, key: Any) -> bool:
-    return type(key) is slice
-
-
-@getitem.overload(False)
-def getitem(data: tuple, key: Any) -> int:
-    i: int = operator.index(key)
-    ans: int = getitem_int(data, i)
-    return ans
-
-
-@getitem.overload(True)
-def getitem(data: tuple, key: Any) -> list:
-    r: range = ranging.torange(key, len(data))
-    f: partial = partial(getitem_int, data)
-    m: map = map(f, r)
-    ans: list = list(m)
+def getitem(data: tuple, key: SupportsIndex | slice) -> list:
+    ans: int | tuple
+    if type(key) is slice:
+        r: range = ranging.torange(key, len(data))
+        f: partial = partial(getitem_int, data)
+        ans = list(map(f, r))
+    else:
+        ans = getitem_int(data, operator.index(key))
     return ans
 
 
