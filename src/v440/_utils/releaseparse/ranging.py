d65470b68a5757d606bff68290bb1b96edc7f3c6
Johannes <johannes.programming@gmail.com>
2025-10-05 11:47:52 +0200

    Version 2.0.0.dev177

--- src/v440/_utils/releaseparse/ranging.py
+++ src/v440/_utils/releaseparse/ranging.py
@@ -8,8 +8,8 @@ __all__ = ["torange"]
 
 def torange(key: slice, length: int) -> range:
     step: int = calcstep(key.step)
-    start: int = calcstart(key.start, length=length, fwd=step > 0)
-    stop: int = calcstop(key.stop, length=length, fwd=step > 0)
+    start: int = calcstart(key.start, length=length, bwd=step < 0)
+    stop: int = calcstop(key.stop, length=length, bwd=step < 0)
     ans: range = range(start, stop, step)
     return ans
 
@@ -20,27 +20,25 @@ def calcstep(value: Optional[SupportsIndex]) -> int:
     return operator.index(value)
 
 
-def calcstart(value: Optional[SupportsIndex], *, length: int, fwd: bool) -> int:
-    ans: int
+def calcstart(value: Optional[SupportsIndex], *, length: int, bwd: bool) -> int:
     if value is None:
-        ans = 0 if fwd else (length - 1)
-    else:
-        ans = operator.index(value)
-    if ans < 0:
-        ans += length
-    if ans < 0:
-        ans = 0 if fwd else -1
-    return ans
+        if bwd:
+            return length - 1
+        else:
+            return 0
+    ans: int = operator.index(value)
+    if ans >= 0:
+        return ans
+    return max(-bwd, ans + length)
 
 
-def calcstop(value: Optional[SupportsIndex], *, length: int, fwd: bool) -> int:
-    ans: int
+def calcstop(value: Optional[SupportsIndex], *, length: int, bwd: bool) -> int:
     if value is None:
-        ans = length if fwd else -1
-    else:
-        ans = operator.index(value)
-    if ans < 0:
-        ans += length
-    if ans < 0:
-        ans = 0 if fwd else -1
-    return ans
+        if bwd:
+            return -1
+        else:
+            return length
+    ans: int = operator.index(value)
+    if ans >= 0:
+        return ans
+    return max(-bwd, ans + length)
