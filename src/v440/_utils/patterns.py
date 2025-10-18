73b08f5082eeab80306eb9507d55c1165075620b
Johannes <johannes.programming@gmail.com>
2025-10-18 11:55:29 +0200

    Version 2.0.0+local.56.packaging

--- src/v440/_utils/patterns.py
+++ src/v440/_utils/patterns.py
@@ -4,11 +4,8 @@ from typing import *
 from v440._utils.Cfg import Cfg
 
 QUAL: re.Pattern = re.compile(
-    r"(?P<pre>%s)?(?P<post>%s)?(?P<dev>%s)?"
-    % (
-        Cfg.cfg.data["patterns"]["PRE"],
-        Cfg.cfg.data["patterns"]["POST"],
-        Cfg.cfg.data["patterns"]["DEV"],
+    Cfg.cfg.data["patterns"]["forms"]["qual"].format(
+        **Cfg.cfg.data["patterns"]["atoms"]
     ),
     re.VERBOSE,
 )
