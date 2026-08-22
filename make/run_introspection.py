73007a00ed8d9aefdb23d6b8ba61bf2480af9794
Johannes Programming <johannes.programming@gmail.com>
2026-08-22 19:09:00 +0200

    make

--- make/run_introspection.py
+++ make/run_introspection.py
@@ -1,5 +1,10 @@
 from typing import reveal_type
 
+import datahold
+
+import v440.abc.CoreABC
 import v440.abc.ListABC
 
+reveal_type(datahold.MutableListSlot.copy)
+reveal_type(v440.abc.CoreABC.CoreABC.copy)
 reveal_type(v440.abc.ListABC.ListABC.__type__)
